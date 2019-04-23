b0c7f74744bac93921258e8f52fec689332d1c21
apetrovskiy <apetrovskiy@hotmail.com>
2019-04-23 21:46:15 +0300

    the initial change

--- Lesson_01/HelloActivity/app/src/main/java/ru/geekbrains/HelloActivity.java
+++ Lesson_01/HelloActivity/app/src/main/java/ru/geekbrains/HelloActivity.java
@@ -2,6 +2,9 @@ package ru.geekbrains;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HelloActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        TextView greeting = (TextView) findViewById(R.id.greeting);
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if (5 <= currentHour && currentHour < 12){
+            greeting.setText("Good morning GeekBrain!");
+        }
+        else if (12 <= currentHour && currentHour < 6) {
+            greeting.setText("Good afternoon GeekBrains!");
+        }
+        else if (6 <= currentHour && currentHour < 9) {
+            greeting.setText("Good evening GeekBrains!");
+        }
+        else {
+            greeting.setText("Good night GeekBrains!");
+        }
     }
 }
